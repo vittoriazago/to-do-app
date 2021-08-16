5ea042bed8d8caeb35e06d2fee47150866e29af4
Vittoria Zago <vittoriazago1@hotmail.com>
2021-08-16 20:45:59 -0300

    Configuring middlewares for exceptions

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import { itemsRouter } from "./item/items.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
 
 dotenv.config();
 
@@ -26,9 +28,12 @@ const app = express();
  app.use(helmet());
  app.use(cors());
  app.use(express.json());
- 
+
  app.use("/api/menu/items", itemsRouter);
 
+ app.use(errorHandler);
+ app.use(notFoundHandler);
+ 
 /**
  * Server Activation
  */
